864e106a17e04ac66c861198b0f9b47b37ff1abb
TSprech <trey.sprecher@gmail.com>
2019-09-27 15:55:32 -0400

    Radio Button Parameterized Function
    Wrote a function to cycle through images starting at a set image and going through a predetermined number of images, hiding all except the one that has just been selected.

--- Radio Button Image Fade/Radio Button Image Fade.js
+++ Radio Button Image Fade/Radio Button Image Fade.js
@@ -1,29 +1,37 @@
 // For full API documentation, including code examples, visit http://wix.to/94BuAAs
+
+//These set up the durations
 let FadeOptions = {
 	"duration": 750,
 	"delay": 0
 };
 
+function ImageHide(ActiveNumber, NumOfPhotos, StartPhotoNum) {
+	var ChangeImg; // Used to store the ID of the image to be hidden/shown
+
+	for (var i = StartPhotoNum; i <= NumOfPhotos; i++) {
+        // If the ID of the photo that is to be shown comes up
+        // show that image then skip to the next iteration of hiding
+		if (i == ActiveNumber) {
+			ChangeImg = "#image" + i;
+			console.log("Showing" + ChangeImg);
+			$w(ChangeImg).show("fade", FadeOptions);
+			continue;
+		}
+        // Hide all other images
+		ChangeImg = "#image" + i;
+		console.log("Hiding" + ChangeImg);
+		$w(ChangeImg).hide("fade", FadeOptions);
+	}
+}
+
 $w.onReady(function () {});
 
+//Change the "radioGroup1" to the ID of the radio button group
 export function radioGroup1_change(event) {
-	let RadioValue = $w("#radioGroup1").value;
-	console.log(RadioValue);
-	switch (RadioValue) {
-	case 'Radio button1':
-		$w("#image1").hide("fade", FadeOptions);
-		$w("#image2").show("fade", FadeOptions);
-		$w("#image3").hide("fade", FadeOptions);
-		break;
-	case 'Radio button2':
-		$w("#image1").show("fade", FadeOptions);
-		$w("#image2").hide("fade", FadeOptions);
-		$w("#image3").hide("fade", FadeOptions);
-		break;
-	case 'Radio button3':
-		$w("#image1").hide("fade", FadeOptions);
-		$w("#image2").hide("fade", FadeOptions);
-		$w("#image3").show("fade", FadeOptions);
-		break;
-	}
+	let RadioValue = $w("#radioGroup1").value; //Number the values of the radio buttons to make it simpler
+	console.log("Radio Button Group Value: " + RadioValue);
+    
+    //Call the hiding and showing image funciton
+	ImageHide(RadioValue, 3, 1);
 }
