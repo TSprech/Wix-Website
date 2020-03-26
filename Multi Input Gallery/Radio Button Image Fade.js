de94f3ba3d231651f80c0bc1910af91eb6f29f06
TSprech <trey.sprecher@gmail.com>
2020-03-26 18:48:59 -0400

    Working Class
    Working class code to make setting up radio groups and slideshows easier.

--- Multi Input Gallery/Radio Button Image Fade.js
+++ Multi Input Gallery/Radio Button Image Fade.js
@@ -1,30 +1,55 @@
-class RadioSlideshow{
-	constructor(slideshow_name, radio_button_group_name){
+class Radio_Slideshow {
+	constructor(slideshow_name, radio_button_group_name) {
 		this._radio_button_group_id = "#" + $w(radio_button_group_name).id;
 		this._slideshow_id = "#" + $w(slideshow_name).id;
+
+		var _current_radio_value = 0;
 	}
 
 	SwitchSlides(activeNumber) {
 		$w(this._slideshow_id).changeSlide(activeNumber);
 	}
 
-	get_radio_button_group_id(){
+	update_current_radio_value() {
+		this._current_radio_value = $w(this._radio_button_group_id).value;
+		console.log(this._current_radio_value);
+		this._new_radio_set_value = parseInt(this._current_radio_value, 10);
+		$w(this._slideshow_id).changeSlide(this._new_radio_set_value);
+	}
+
+	get_radio_button_group_id() {
 		return this._radio_button_group_id;
 	}
 
-	get_slideshow_id(){
+	get_slideshow_id() {
 		return this._slideshow_id;
 	}
 }
 
+var control_panel_slide_group;
+
 $w.onReady(function () {
-
-//New radio button group controlled slideshow
-let control_panel_slide_group = new RadioSlideshow("#controlPanelSlideshow", "#controlPanelRadio");
-
-control_panel_slide_group.SwitchSlides(3);
-
+	//New radio button group controlled slideshow
+	control_panel_slide_group = new Radio_Slideshow("#controlPanelSlideshow", "#controlPanelRadio");
 });
+
+export function controlPanelRadio_change_1(event) {
+	control_panel_slide_group.update_current_radio_value();
+	console.log("Radio Change\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -54,9 +79,9 @@ export function controlPanelRadio_change_1(event) {
 	let radioValue = $w(radioId).value; //Number the values of the radio buttons to make it simpler
 	console.log(radioValue);
 	radioNum = parseInt(radioValue, 10);
-		if (radioModifier){
-			radioNum += 4;
-		}
+	if (radioModifier) {
+		radioNum += 4;
+	}
 	SwitchSlides(radioNum);
 }
 
